2a980c9359ed3937623d78ed1fae665c4b1d25a5
Fabian Vogelsteller <fabian@frozeman.de>
2014-08-27 14:49:44 +0200

    made 0.9.0 ready

--- package.js
+++ package.js
@@ -1,18 +1,16 @@
 Package.describe({
-    summary: "A tool for making and composing asynchronous promises in JavaScript http://documentup.com/kriskowal/q/"
+	name: "mrt:q",
+    summary: "A wrapper for the Q promise library from kris kowal",
+  	version: "1.0.1",
+    git: "https://github.com/frozeman/q-meteor.git"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
+    api.versionsFrom('METEOR@0.9.0');
 
     // EXPORT
     api.export('Q');
 
     // FILES
-    api.add_files('q.js', ['client', 'server']);
+    api.addFiles('lib/q-1/q.js', ['client', 'server']);
 });
-
-// Package.on_test(function (api) {
-//   api.use('q');
-//   api.use('tinytest');
-//   api.add_files('q_tests.js', ['client', 'server']);
-// });
